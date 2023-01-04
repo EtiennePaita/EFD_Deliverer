5e4cc9e904220d0af498afe2d915b477762a6ac6
Etienne Païta <etiennepaita1@gmail.com>
2023-01-04 17:43:46 +0100

    Initial Commit

---
+++ EFD_DelivererUITests/EFD_DelivererUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EFD_DelivererUITestsLaunchTests.swift
+//  EFD_DelivererUITests
+//
+//  Created by Etienne Païta on 04/01/2023.
+//
+
+import XCTest
+
+final class EFD_DelivererUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

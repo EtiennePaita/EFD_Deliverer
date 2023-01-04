ae82028f0bee29fc0d9478e61c4b922060c9a002
Etienne Païta <etiennepaita1@gmail.com>
2023-01-04 17:47:16 +0100

    initial commit

---
+++ EFD_Deliverer/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  EFD_Deliverer
+//
+//  Created by Etienne Païta on 04/01/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
